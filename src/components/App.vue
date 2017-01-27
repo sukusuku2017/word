acb74f5525fd321e6e2317af655faad3d3f9f416
GimYoungPhil <youngphil.gim@gmail.com>
2017-01-27 23:47:36 +0900

    set vue

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="sukusuku-app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'SukuSuku 2017'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
