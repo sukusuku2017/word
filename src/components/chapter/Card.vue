135cf5e40ab876ee118df05a5bd04eea2d08c43b
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-02 16:02:05 +0900

    add test Card

---
+++ src/components/chapter/Card.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        Component
+      </p>
+      <a class="card-header-icon">
+        <span class="icon">
+          <i class="fa fa-angle-down"></i>
+        </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+        <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+        <br>
+        <small>11:09 PM - 1 Jan 2016</small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item">Save</a>
+      <a class="card-footer-item">Edit</a>
+      <a class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
