3bf54c2c7e6585198f9600ab188da971abc18a6a
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-07 21:51:34 +0900

    fixed Home

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -5,9 +5,6 @@
     <list></list>
     <hr class="is-maginless">
     <card></card>
-    <hr class="is-maginless">
-    <state></state>
-    <hr class="is-maginless">
   </div>
 </template>
 
@@ -16,11 +13,11 @@ import Hero from './home/Hero.vue'
 import Feature from './home/Feature.vue'
 import List from './home/List.vue'
 import Card from './home/Card.vue'
-import State from './home/State.vue'
+
 
 export default {
 
-  components: { Hero, Feature, List, Card, State }
+  components: { Hero, Feature, List, Card }
 }
 </script>
 
