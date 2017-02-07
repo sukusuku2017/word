148266bbaffefebaa86f9de87793ece5f1a941c7
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-07 12:28:27 +0900

    fixed AppTable

--- app/components/course/AppTable.vue
+++ app/components/course/AppTable.vue
@@ -14,14 +14,24 @@
         <td>{{ record.st }}</td>
         <td>
           {{ record.wd }}
-          <router-link :to="`/list/${record.ch}`"
-              class="button is-primary is-outlined">
-            L
-          </router-link>
-          <router-link :to="`/card/${record.ch}`"
-              class="button is-primary is-outlined">
-            C
-          </router-link>
+          <template v-if="record.wd">
+            <router-link :to="`/list/${record.ch}`"
+                class="button is-primary is-outlined">
+              L
+            </router-link>
+            <router-link :to="`/card/${record.ch}`"
+                class="button is-primary is-outlined">
+              C
+            </router-link>
+          </template>
+          <template v-else>
+            <button class="button is-primary is-outlined is-disabled">
+              L
+            </button>
+            <button class="button is-primary is-outlined is-disabled">
+              C
+            </button>
+          </template>
         </td>
         <td>{{ record.ud }}</td>
       </tr>
