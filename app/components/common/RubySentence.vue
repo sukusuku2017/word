31ef897707c59f2a0729b8297c664cf92851f7aa
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-11 21:25:53 +0900

    fixed sentence

--- app/components/common/RubySentence.vue
+++ app/components/common/RubySentence.vue
@@ -1,23 +1,11 @@
 <template lang="html">
   <p class="ruby-sentence">
-    <span v-if="word.base && !word.ruby">
-      {{ word.base }}
-    </span>
-    <ruby v-else-if="word.base && word.ruby">
-      {{ word.base }}
-      <rp>(</rp><rt>{{ word.ruby }}</rt><rp>)</rp>
-    </ruby>
-    <template v-else-if="word.characters">
-      <template v-for="ch in word.characters">
-        <span v-if="ch.base && !ch.ruby">{{ ch.base }}</span>
-        <ruby v-else-if="ch.base && ch.ruby">
-          {{ ch.base }} <rp>(</rp><rt>{{ ch.ruby }}</rt><rp>)</rp>
-        </ruby>
-      </template>
+    <template v-for="ch in word.characters">
+      <span v-if="ch.base && !ch.ruby">{{ ch.base }}</span>
+      <ruby v-else-if="ch.base && ch.ruby">
+        {{ ch.base }} <rp>(</rp><rt>{{ ch.ruby }}</rt><rp>)</rp>
+      </ruby>
     </template>
-    <span v-else>
-      Data Error
-    </span>
   </p>
 </template>
 
