c779943c378466de5051ee513b6c1eeca640f85b
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-15 02:04:39 +0900

    add st21.json

--- app/components/app/AppFooter.vue
+++ app/components/app/AppFooter.vue
@@ -4,17 +4,17 @@
       <div class="content has-text-left">
         <p>Colophon</p>
         <p>
-          Built with <a href="https://vuejs.org/">Vue.js</a>,
-          <a href="http://bulma.io/">Bulma</a> and
-          served on <a href="https://github.com/sukusuku2017">GitHub</a>,
-          <a href="http://surge.sh/">Surge</a>.
+          Built with <a href="https://vuejs.org/" target='_blank'>Vue.js</a>,
+          <a href="http://bulma.io/" target='_blank'>Bulma</a> and
+          served on <a href="https://github.com/sukusuku2017" target='_blank'>GitHub</a>,
+          <a href="http://surge.sh/" target='_blank'>Surge</a>.
           <br/>
           Text is set in Google Font’s
-          <a href="https://googlefonts.github.io/japanese/">Japanese Early Access</a>
+          <a href="https://googlefonts.github.io/japanese/" target='_blank'>Japanese Early Access</a>
         </p>
         <p>
           <em>Copyright © 2017</em>
-          <a href="https://github.com/GimYoungPhil">Gim YoungPhil</a>
+          <a href="https://github.com/GimYoungPhil" target='_blank'>Gim YoungPhil</a>
         </p>
         <a href='https://ko-fi.com/A864L8S' target='_blank'>
           <img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi4.png?v=f' border='0' alt='Buy Me a Coffee at ko-fi.com' />
