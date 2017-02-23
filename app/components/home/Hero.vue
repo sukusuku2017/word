894fcd773025cebf766213ad8cf25e464e4ccfd8
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-24 02:16:29 +0900

    add Youtube

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -1,20 +1,30 @@
 <template lang="html">
   <section class="hero is-medium">
     <div class="hero-body">
-      <div class="container has-text-centered">
-        <h1 class="title">
-          すくすく
-        </h1>
-        <h2 class="subtitle">
-          日本語
-        </h2>
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column">
+            <h1 class="title is-1">
+              すくすく
+            </h1>
+            <h2 class="subtitle is-2">
+              日本語
+            </h2>
+          </div>
+          <div class="column-narrow">
+            <youtube></youtube>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import Youtube from './Youtube.vue'
+
 export default {
+  components: { Youtube }
 }
 </script>
 
---
+++ app/components/home/Youtube.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div class="card card-youtube">
+    <a href="https://youtu.be/lfT8IU0so24" target="_blank">
+      <div class="card-content">
+        <div class="image-block">
+          <figure class="image">
+            <img src="./ljh.png" alt="Image">
+          </figure>
+          <div class="image-block-label">
+            <img src="./yt-icon.png" alt="Image">
+            학습 비법 노하우
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+.card-youtube {
+  width: 340px;
+  border-radius: 7px;
+}
+
+.card-content {
+  padding: 1rem;
+}
+
+.image-block {
+  position: relative;
+}
+
+.image-block-label {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: #000;
+  color: #fff;
+  opacity: .7;
+  text-align: center;
+  font-size: 1.5rem;
+}
+
+.image-block-label img {
+  width: 26px;
+}
+</style>
